c7afbf279284eddfdcfca9c4f2a599e69d95659e
charlie <pancakes2425@gmail.com>
2024-11-27 12:56:44 -0600

    Version 1 de la practica 4

---
+++ Problema14.js
@@ -0,0 +1,14 @@
+let daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+
+
+daysOfWeek.shift();
+
+
+daysOfWeek.pop();
+
+console.log("Después de eliminar el primer y último elemento:", daysOfWeek); 
+
+
+daysOfWeek.push('Sat', 'Sun');
+
+console.log("Después de agregar 'Sat' y 'Sun':", daysOfWeek); 
